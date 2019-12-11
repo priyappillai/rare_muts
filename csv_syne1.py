bd78cdc170a0e060e524cb9d6c0e4ae6492a7293
priyappillai <pppillai@mit.edu>
2019-12-11 09:50:36 -0500

    read in vcf file

--- csv_syne1.py
+++ csv_syne1.py
@@ -1,14 +1,34 @@
 import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
+#import hail as hl
 import allel
 
 
-callset = allel.read_vcf('gnomad.exomes.r2.1.1.sites.22.vcf', fields=['variants/vep'])#, 'variants/variant_type', 'variants/allele_type', 'variants/AC', 'variants/AF'])
+pops = ['afr', 'amr', 'asj', 'eas', 'fin', 'nfe', 'sas']
+afpops = ['variants/AF_%s' % pop for pop in pops]
+'''
+ds = hl.import_vcf('gnomad.exomes.r2.1.1.sites.22.vcf.bgz')
+print(ds.count_rows())
+print(ds.col()[:20])
+'''
+callset = allel.read_vcf('gnomad.exomes.r2.1.1.sites.22.vcf', fields=['PASS', 'ID', 'REF', 'ALT', 'variants/vep', 'variants/AF']+afpops)#, 'variants/variant_type', 'variants/allele_type', 'variants/AC', 'variants/AF'])
+#df_allel = pd.DataFrame(callset)
+n_genes = len(callset['variants/ID'])
+print(callset['variants/ID'][:10])
+print(callset['variants/REF'][:10])
+print(callset['variants/ALT'][:10])
+print(callset['variants/FILTER_PASS'][:10])
+print(callset['variants/AF'][:10])
+print(callset['variants/AF_asj'][:10])
+print(callset['variants/vep'][:10])
 
-df = pd.read_csv("gnomAD_v2.1.1_ENSG00000131018_2019_11_25_03_29_39.csv")
+for i in range(n_genes):
+  pass
 
-pops = ["African", "Latino", "Ashkenazi Jewish", "East Asian", "European (Finnish)", "European (non-Finnish)", "Other", "South Asian"]
+#df = pd.read_csv("gnomAD_v2.1.1_ENSG00000131018_2019_11_25_03_29_39.csv")
+
+namepops = ["African", "Latino", "Ashkenazi Jewish", "East Asian", "European (Finnish)", "European (non-Finnish)", "South Asian"]
 
 annotations = ['3_prime_UTR_variant', 'stop_retained_variant',
        'synonymous_variant', 'missense_variant', 'inframe_insertion',
@@ -55,7 +75,7 @@ nonsyn_code = ['missense_variant', 'inframe_insertion', 'stop_gained',
 is_nonsyn_code = (df.Annotation.isin(nonsyn_code))
 nonsyn_code_rows = df[is_nonsyn_code]
 
-syn_noncode = ['3_prime_UTR_variant', 'intron_variant', '5_prime_UTR_variant']
+syn_noncode = ['3_prime_UTR_variant', 'intron_variant', '5_prime_UTR_variant', 'downstream_gene_variant']
 is_syn_noncode = (df.Annotation.isin(syn_noncode) | \
                  ((df.Annotation == 'splice_region_variant') & \
                   (df["Protein Consequence"].isna())))
