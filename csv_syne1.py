f8527b1f1e5b87ca753bb7d820e437e5543e64dd
priyappillai <pppillai@mit.edu>
2019-12-15 16:46:32 -0500

    Fully fixed code for all data

--- csv_syne1.py
+++ csv_syne1.py
@@ -14,13 +14,14 @@ variant_types = ["splice", "start_lost", "stop_lost", "stop_gained", "frameshift
                  "inframe_insertion", "inframe_deletion", "missense_variant", "UTR", 
                  "synonymous", "stop_retained", "intron", "downstream", "upstream", 
                  "protein_altering", "non_coding", "intergenic", "regulatory_region", 
-                 "incomplete_terminal_codon", "mature_miRNA"]
+                 "incomplete_terminal_codon", "mature_miRNA", "protein_coding", 
+                 "coding_sequence"]
 
 nonsyn_coding_variants = ["start_lost", "stop_lost", "stop_gained", "frameshift", 
                           "inframe_insertion", "inframe_deletion", "missense_variant", 
-                          "protein_altering"]
+                          "protein_altering", "protein_coding"]
 
-syn_coding_variants = ["synonymous", "stop_retained"]
+syn_coding_variants = ["synonymous", "stop_retained", "coding_sequence"]
 
 noncoding_variants = ["intron", "downstream", "upstream", "non_coding", "intergenic"]
 
@@ -28,70 +29,109 @@ regulatory_variants = ["UTR", "regulatory_region", "mature_miRNA"]
 
 splice_variants = ["splice"]
 
-def vcf_read(filename, picklefile = None):
-  with open(filename, 'r') as f:
-    for _ in range(900):
-      f.readline()
-    variants = []
-    for line in f:
-      tab_line = line.split('\t')
-      chrm = tab_line[0]
-      rsid = tab_line[2]
-      ref = tab_line[3]
-      alt = tab_line[4]
-      filt_pass = tab_line[6]
-      var_line = tab_line[7].split(';')
+chrm_list = [str(i+1) for i in range(22)] + ["X", "Y"]
 
-      ac = float(var_line[0].split("=")[1])
-      ac_afr = float(var_line[78].split("=")[1])
-      ac_amr = float(var_line[158].split("=")[1])
-      ac_asj = float(var_line[586].split("=")[1])
-      ac_eas = float(var_line[177].split("=")[1])
-      ac_fin = float(var_line[526].split("=")[1])
-      ac_nfe = float(var_line[494].split("=")[1])
-      ac_sas = float(var_line[121].split("=")[1])
+def vcf_read(filename, all_chrm = False, picklefile = None, skip = 0, pre_chrm = 0):
+  try:
+    with open(filename, 'r') as f:
+      j = 0
+      for _ in range(900+skip):
+        if _ >= 900:
+          j += 1
+        next(f)
+        if (j>0 and j%100000 == 0):
+          print("%i skipped" %j)
+      all_variants = [] if not all_chrm else {chrm: [] for chrm in chrm_list}
+      i = skip
+      prev_chrm = 0
+      for line in f:
+        tab_line = line.split('\t')
+        chrm = tab_line[0]
+        rsid = tab_line[2]
+        ref = tab_line[3]
+        alt = tab_line[4]
+        filt_pass = tab_line[6]
+        var_line = tab_line[7].split(';')
+        var_dict = {var_i.split("=")[0]: var_i.split("=")[1] for var_i in var_line if "=" in var_i}
 
-      an = float(var_line[1].split("=")[1])
-      an_afr = float(var_line[79].split("=")[1])
-      an_amr = float(var_line[159].split("=")[1])
-      an_asj = float(var_line[587].split("=")[1])
-      an_eas = float(var_line[178].split("=")[1])
-      an_fin = float(var_line[527].split("=")[1])
-      an_nfe = float(var_line[495].split("=")[1])
-      an_sas = float(var_line[122].split("=")[1])
+        ac = float(var_dict["AC"])
+        ac_afr = float(var_dict["AC_afr"])
+        ac_amr = float(var_dict["AC_amr"])
+        ac_asj = float(var_dict["AC_asj"])
+        ac_eas = float(var_dict["AC_eas"])
+        ac_fin = float(var_dict["AC_fin"])
+        ac_nfe = float(var_dict["AC_nfe"])
+        ac_sas = float(var_dict["AC_sas"])
 
-      af = float(var_line[2].split("=")[1])
-      af_afr = float(var_line[80].split("=")[1])
-      af_amr = float(var_line[160].split("=")[1])
-      af_asj = float(var_line[588].split("=")[1])
-      af_eas = float(var_line[179].split("=")[1])
-      af_fin = float(var_line[528].split("=")[1])
-      af_nfe = float(var_line[496].split("=")[1])
-      af_sas = float(var_line[123].split("=")[1])
+        an = float(var_line[1].split("=")[1])
+        an_afr = float(var_dict["AN_afr"])
+        an_amr = float(var_dict["AN_amr"])
+        an_asj = float(var_dict["AN_asj"])
+        an_eas = float(var_dict["AN_eas"])
+        an_fin = float(var_dict["AN_fin"])
+        an_nfe = float(var_dict["AN_nfe"])
+        an_sas = float(var_dict["AN_sas"])
 
-      variant_info = var_line[-1]
-      variant = None
-      for variant_type in variant_types:
-        if variant_type in variant_info:
-          variant = variant_type
-      if variant == None:
-        print(var_line)
-        raise ValueError
+        '''
+        af = float(var_line[2].split("=")[1])
+        af_afr = float(var_dict["AF_afr"])
+        af_amr = float(var_dict["AF_amr"])
+        af_asj = float(var_dict["AF_asj"])
+        af_eas = float(var_dict["AF_eas"])
+        af_fin = float(var_dict["AF_fin"])
+        af_nfe = float(var_dict["AF_nfe"])
+        af_sas = float(var_dict["AF_sas"])
+        '''
 
-      variants.append(dict(chrm=chrm, rsid=rsid, ref=ref, alt=alt, filt_pass=filt_pass,
-                           ac=ac, ac_afr=ac_afr, ac_amr=ac_amr, ac_asj=ac_asj, 
-                           ac_eas=af_eas, ac_fin=af_fin, ac_nfe=ac_nfe, ac_sas=ac_sas,
-                           an=an, an_afr=an_afr, an_amr=an_amr, an_asj=an_asj, 
-                           an_eas=an_eas, an_fin=an_fin, an_nfe=an_nfe, an_sas=an_sas,
-                           af=af, af_afr=af_afr, af_amr=af_amr, af_asj=af_asj, 
-                           af_eas=af_eas, af_fin=af_fin, af_nfe=af_nfe, af_sas=af_sas, 
-                           variant=variant))
+        variant_info = var_line[-1]
+        variant = None
+        for variant_type in variant_types:
+          if variant_type in variant_info:
+            variant = variant_type
+        if variant == None:
+          print(var_line)
+          variant = input("Input variant type")
 
-  df = pd.DataFrame(variants)
-  if picklefile:
+        this_variant = dict(chrm=chrm, rsid=rsid, ref=ref, alt=alt, filt_pass=filt_pass,
+                            ac=ac, ac_afr=ac_afr, ac_amr=ac_amr, ac_asj=ac_asj, 
+                            ac_eas=ac_eas, ac_fin=ac_fin, ac_nfe=ac_nfe, ac_sas=ac_sas,
+                            an=an, an_afr=an_afr, an_amr=an_amr, an_asj=an_asj, 
+                            an_eas=an_eas, an_fin=an_fin, an_nfe=an_nfe, an_sas=an_sas,
+                            #af=af, af_afr=af_afr, af_amr=af_amr, af_asj=af_asj, 
+                            #af_eas=af_eas, af_fin=af_fin, af_nfe=af_nfe, af_sas=af_sas, 
+                            variant=variant)
+
+        if all_chrm:
+          all_variants[chrm].append(this_variant)
+          if prev_chrm != chrm:
+            if picklefile and (prev_chrm in chrm_list[pre_chrm:]):
+              df = pd.DataFrame(all_variants[prev_chrm])
+              pickle.dump(df, open(prev_chrm+picklefile,'wb'))
+              print(prev_chrm + " done!")
+            prev_chrm = chrm
+        else:
+          all_variants.append(this_variant)
+
+        i += 1
+        if (i%100000) == 0:
+          print("%i variants" %i)
+
+  except Exception as e:
+    print(i)
+    print()
+    print(var_line)
+    raise e
+
+  if all_chrm:
+    if picklefile:
+      df = pd.DataFrame(all_variants[chrm_list[-1]])
+      pickle.dump(df, open(chrm_list[-1]+picklefile,'wb'))
+      print(prev_chrm + " done!")
+  elif picklefile:
+    df = pd.DataFrame(all_variants)
     pickle.dump(df, open(picklefile,'wb'))
-  return df
 
+  return all_variants
 
 
 def vcf_rows(df):
@@ -112,9 +152,9 @@ def vcf_rows(df):
   is_splice = ((df.variant.isin(splice_variants) & (df.filt_pass == 'PASS')))
   splice_rows = df[is_splice]
 
-  #print(len(df))
-  #print(len(syn_rows))
-  #print(len(nonsyn_code_rows))
+  print(len(df))
+  print(len(syn_rows))
+  print(len(nonsyn_code_rows))
 
   return syn_rows, nonsyn_code_rows
 
@@ -140,6 +180,7 @@ splice = ['splice_region_variant',
           'splice_acceptor_variant']
 
 syn_code = ['synonymous_variant',
+            'coding_sequence_variant',
             'stop_retained_variant']
 
 utr = ['3_prime_UTR_variant',
@@ -153,7 +194,6 @@ nonsyn_code = ['missense_variant',
                'inframe_deletion',
                'inframe_insertion',
                'protein_altering_variant',
-               'coding_sequence_variant',
                'stop_lost']
 
 
@@ -178,9 +218,9 @@ def csv_read_rows(filename):
   is_syn = ((is_syn_code | is_intron) | is_utr)
   syn_rows = df[is_syn]
   
-  #print(len(df))
-  #print(len(syn_rows))
-  #print(len(nonsyn_code_rows))
+  print(len(df))
+  print(len(syn_rows))
+  print(len(nonsyn_code_rows))
 
   return syn_rows, nonsyn_code_rows
 
@@ -245,16 +285,24 @@ def vcf_graph(syn_rows, nonsyn_code_rows, chrm, binsize):
     fig.show()
     fig.write_image("Chromosome %s bin=%1.2f (%s).png" %(chrm, binsize, namepops[i]))
 
-#df = vcf_read('gnomad.exomes.r2.1.1.sites.22.vcf', '22.pickle')
-df = pickle.load(open('22.pickle', 'rb'))
-syn_rows, nonsyn_code_rows = vcf_rows(df)
-for binsize in [.1,.25, .5, 1]:
-  vcf_graph(syn_rows, nonsyn_code_rows, '22', binsize)
 
-lct_syn, lct_nonsyn = csv_read_rows("gnomAD_v2.1.1_ENSG00000115850_2019_12_13_19_29_29.csv")
-syne1_syn, syne1_nonsyn = csv_read_rows("gnomAD_v2.1.1_ENSG00000131018_2019_11_25_03_29_39.csv")
+if __name__ == "__main__":
+  #df = vcf_read(r'E:\Fall2019\HST508\final_proj\gnomad.exomes.r2.1.1.sites.vcf\gnomad.exomes.r2.1.1.sites.vcf', 
+  #              all_chrm = True, picklefile='_all.pickle', skip = 0, pre_chrm = 0)
+  df = vcf_read(r'E:\Fall2019\HST508\final_proj\gnomad.exomes.r2.1.1.sites.Y.vcf', 
+                all_chrm = False, picklefile='Y.pickle', skip = 0, pre_chrm = 0)
+  #df = pickle.load(open('22.pickle', 'rb'))
+  #syn_rows, nonsyn_code_rows = vcf_rows(df)
+  #for binsize in [.1,.25, .5, 1]:
+    #vcf_graph(syn_rows, nonsyn_code_rows, 'all', binsize)
 
-for binsize in [.1,.25, .5, 1]:
-  csv_graph(lct_syn, lct_nonsyn, "LCT", binsize)
-  csv_graph(syne1_syn, syne1_nonsyn, "SYNE1", binsize)
+  '''
+  lct_syn, lct_nonsyn = csv_read_rows("gnomAD_v2.1.1_ENSG00000115850_2019_12_13_19_29_29.csv")
+  syne1_syn, syne1_nonsyn = csv_read_rows("gnomAD_v2.1.1_ENSG00000131018_2019_11_25_03_29_39.csv")
+
+  for binsize in [.1,.25, .5, 1]:
+    csv_graph(lct_syn, lct_nonsyn, "LCT", binsize)
+    csv_graph(syne1_syn, syne1_nonsyn, "SYNE1", binsize)
+
+  '''
 
